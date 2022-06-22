f3fa6409b7b749b629502ae9c2be590b14223afb
Joe <hutcheson2001@yahoo.com>
2022-06-22 06:26:33 -0400

    commit tree 43c667e736da9fcb1ce530d87b70956d0eaaeb58

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,5 +1,41 @@
 require_relative './node'
+require "pry"
 
 class LinkedList
+  attr_accessor :head
 
+  def initialize
+    @head = nil
+  end
+
+  def prepend(node)
+    if head.nil?
+      self.head = node
+    end
+      return 
+  end
+  
+  def append(node)
+    if head.nil?
+      self.head = node
+      return
+    end
+
+    last_node = head
+
+    while last_node.next_node
+      last_node = last_node.next_node
+    end
+
+    last_node.next_node = node
+  end
+    
+  def delete_head 
+
+  end
+
+  def delete_tail
+    self.pop
+  end
+    
 end
